e8622f334f74eebc7b6fac9b3d91f1e3707ddd2a
Md. Naimur Zaman Sunny <egmdsunny@gmail.com>
2022-09-16 23:25:52 +0600

    Modify

--- Snag Repeted Customer data.sql
+++ Snag Repeted Customer data.sql
@@ -40,7 +40,9 @@ from (select pdt.DamageCustomer, COUNT(pdt.DamageCustomer) Damage
 	having COUNT(pdt.DamageCustomer) = 3
 ) last3week
 join (
-	select customerid DamageCustomer, sum(saleprice) DamageAmount
+	select customerid DamageCustomer,
+	       sum(saleprice) DamageAmount
+		   
 	from ThingRequest tr
 	join shipment s on s.id = tr.shipmentid
 	join [order] o on o.id = s.orderid
